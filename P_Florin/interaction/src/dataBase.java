f1d646dca78430d4b60761dae9c376287e0479ef
Thea <pandelefl23@gmail.com>
2026-07-03 19:00:43 +0300

    Completare pentru voturi si comentarii

---
+++ P_Florin/interaction/src/dataBase.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class dataBase {
+
+    public static List<Post> mockPosts = new ArrayList<>();
+    public static String currentLoggedInUser = "FlorinUser";
+    public static int nextCommentId = 1;
+
+}
