d88a8f7f8d07ee4aad9f9de5ac168e068a9e2bbc
RARES AURELIAN RADUCANU <raducanurares40@gmail.com>
2026-07-10 13:57:05 +0300

    started on continuos running

--- App/App/src/community/SubredditQuery.java
+++ App/App/src/community/SubredditQuery.java
@@ -1,12 +1,22 @@
 package community;
 
+import java.util.Objects;
+
 public class SubredditQuery {
     public void subredditQuery() {
-        String subredditName = SubName.ask();
-        String subredditDescription = SubDescription.ask();
-
-        Subreddit subreddit = new Subreddit(subredditName, subredditDescription);
-        SubredditCreator.saveSubreddit(subreddit);
+        boolean running = true;
+        while(running) {
+            System.out.println("Enter data or 0 to end the program");
+            String subredditName = SubName.ask();
+            String subredditDescription = SubDescription.ask();
+            if(Objects.equals(subredditName, "0") || Objects.equals(subredditDescription, "0")){
+                running = false;
+                System.out.println("Program ended");
+                continue;
+            }
+            Subreddit subreddit = new Subreddit(subredditName, subredditDescription);
+            SubredditCreator.saveSubreddit(subreddit);
+        }
     }
 
 }
