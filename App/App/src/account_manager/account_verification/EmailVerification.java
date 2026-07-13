d260cc0112b33783b009f009a7b635467b51ff89
RARES AURELIAN RADUCANU <raducanurares40@gmail.com>
2026-07-13 12:20:12 +0300

    improved menu nav

--- App/App/src/account_manager/account_verification/EmailVerification.java
+++ App/App/src/account_manager/account_verification/EmailVerification.java
@@ -9,6 +9,7 @@ public class EmailVerification implements AccountVerifier {
         int index1 = email.indexOf("@");
         int index2 = email.indexOf(".");
         if(index1 == -1 || index2 == -1 || index2 - index1 < 2){
+            System.out.println("Email is not valid");
             return false;
         }
         return true;
--- App/App/src/posting/commands/CreatePostCommand.java
+++ App/App/src/posting/commands/CreatePostCommand.java
@@ -18,26 +18,31 @@ public class CreatePostCommand {
     public void execute() {
         boolean running = true;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Are sure you want to create a new post? (y/n)");
-        String answer = scanner.nextLine();
-        if(answer.equals("n")){
-            running = false;
-        }
-        if(running) {
-            System.out.println("CREATE A NEW POST (or enter 0 in any field to return to previous menu):");
-            String title = view.askForTitle();
-            if (title.equals("0")) return;
-            String subreddit = view.askForSubreddit();
-            if (subreddit.equals("0")) return;
-            String baseContent = view.askForContent();
-            if (baseContent.equals("0")) return;
-            String attachment = view.askForAttachment();
+        while (running) {
+            System.out.println("Do you want to create a new post? (y/n)");
+            String answer = scanner.nextLine().toLowerCase();
+            if (answer.equals("n")) {
+                running = false;
+            } else if (!answer.equals("y")) {
+                System.out.println("Invalid input. Please enter 'y' or 'n'.");
+                return;
+            }
+            if (running) {
+                System.out.println("CREATE A NEW POST (or enter 0 in any field to return to the previous menu):");
+                String title = view.askForTitle();
+                if (title.equals("0")) continue;
+                String subreddit = view.askForSubreddit();
+                if (subreddit.equals("0")) continue;
+                String baseContent = view.askForContent();
+                if (baseContent.equals("0")) continue;
+                String attachment = view.askForAttachment();
 
-            String finalContent = baseContent + attachment;
+                String finalContent = baseContent + attachment;
 
-            Post myNewPost = postService.createPost("rares0208", title, finalContent, subreddit);
+                Post myNewPost = postService.createPost("rares0208", title, finalContent, subreddit);
 
-            view.displayPost(myNewPost);
+                view.displayPost(myNewPost);
+            }
         }
     }
 }
